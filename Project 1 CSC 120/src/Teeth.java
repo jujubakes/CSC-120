d16c4af3fbcdabfbcc9da192b33ec0c55bdbacce
Julien Bacon <jxb5207@miami.edu>
2021-10-04 10:44:09 -0400

    created project

---
+++ Project 1 CSC 120/src/Teeth.java
@@ -0,0 +1,19 @@
+public class Teeth {
+
+    private String name;
+    private int b;
+    private int c;
+    private int m;
+    private char[] teeth;
+
+
+    public Teeth(){
+        b = 0;
+        c = 0;
+        m = 0;
+        teeth = new char[0];
+        name = "";
+    }
+
+}
+
